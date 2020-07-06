9aaae4275739038342151d629fadf22b12319bc9
ShinjiroMoriya <jiromo.jiromo.jiromo@gmail.com>
2020-07-06 12:26:57 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# CSG API For LWC
+
+## Description
+Application that can use CSG API from LWC
+
+## Heroku Button
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Language
+- Python 3.8.3
+
+## Framework
+- FastAPI v0.58.0
+
+## Heroku Config Vars
+- SALESFORCE_URL: required -> string
+- QUIP_ACCESS_TOKEN: required -> string
+- QUIP_CLIENT_ID: required -> string
+- QUIP_CLIENT_SECRET: required -> string
+- DOCS_FLAG: optional -> bool True/False
+
---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+import cloudinary
+from os.path import join, dirname
+from cloudinary import uploader
+from cloudinary.api import delete_resources
+from urllib.parse import urlparse
+from quip import QuipClient, get_html
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import bleach
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DOCS_FLAG = os.environ.get('DOCS_FLAG') == 'True'
+
+config = {
+    'title': "CSG API for LWC",
+    'redoc_url': '/'
+}
+
+if DOCS_FLAG is False:
+    config.update({
+        'docs_url': None,
+        'redoc_url': None,
+    })
+
+app = FastAPI(**config)
+
+origins = [os.environ.get('SALESFORCE_URL', '*')]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+class Image(BaseModel):
+    public_id: str
+
+
+cloudinary_parse_url = urlparse(os.environ.get('CLOUDINARY_URL'))
+
+cloudinary.config(
+    cloud_name=cloudinary_parse_url.hostname,
+    api_key=cloudinary_parse_url.username,
+    api_secret=cloudinary_parse_url.password,
+)
+
+
+@app.post('/api/upload')
+def api_upload(upload_file: UploadFile = File(...)):
+    res = uploader.upload(file=upload_file.file)
+    res.update({
+        'cloudinary_url': f'https://res.cloudinary.com/{cloudinary_parse_url.hostname}'
+    })
+    return res
+
+
+@app.post('/api/delete')
+def api_delete(public_id: str = Form(...)):
+    res = delete_resources(public_ids=[public_id])
+    return res
+
+
+@app.get('/api/quip')
+def api_quip(quip_id: str):
+    quip = QuipClient()
+    thread = quip.get_thread(quip_id)
+    html = get_html(thread)
+    return {
+        'html': html,
+        'word': bleach.clean(html, strip=True, tags=['*']).replace('\n', '')
+    }
